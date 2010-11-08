28c71a0d21e7b53efccc4b02ed81d9c0801703e7
rkdevel <rkdevel@ef62a7f8-4c9b-1a64-55d9-32abd1026911>
2010-11-08 22:55:58 +0000

    Daily commit

--- rs232testng.cpp
+++ rs232testng.cpp
@@ -4,6 +4,14 @@ rs232testng::rs232testng(QWidget *parent)
     : QMainWindow(parent)
 {
 	ui.setupUi(this);
+	mainToolBar = new QToolBar();
+	mainToolBar->addAction(ui.actionStart);
+	mainToolBar->addSeparator();
+	mainToolBar->addAction(ui.actionConf);
+	mainToolBar->addAction(ui.actionHelp);
+	mainToolBar->addAction(ui.actionExit);
+
+	ui.mainDock->setWidget(mainToolBar);
 }
 
 rs232testng::~rs232testng()
--- rs232testng.pro
+++ rs232testng.pro
@@ -1,10 +1,9 @@
 TEMPLATE = app
-TARGET = rs232testng 
-
-QT        += core gui 
-
-HEADERS   += rs232testng.h
-SOURCES   += main.cpp \
+TARGET = rs232testng
+QT += core \
+    gui
+HEADERS += rs232testng.h
+SOURCES += main.cpp \
     rs232testng.cpp
-FORMS     += rs232testng.ui    
-RESOURCES +=
+FORMS += rs232testng.ui
+RESOURCES += resources.qrc
